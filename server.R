54dedf458574ccc6020ff30b4a27e6917dbb6a71
Corey Shih <corey.t.shih@gmail.com>
2021-05-05 17:23:48 -0700

    First commit

---
+++ server.R
@@ -0,0 +1,113 @@
+## server.R
+library(recommenderlab)
+library(Matrix)
+
+# define functions
+get_user_ratings = function(value_list) {
+  dat = data.table(MovieID = sapply(strsplit(names(value_list), "_"), 
+                                    function(x) ifelse(length(x) > 1, x[[2]], NA)),
+                   Rating = unlist(as.character(value_list)))
+  dat = dat[!is.null(Rating) & !is.na(MovieID)]
+  dat[Rating == " ", Rating := 0]
+  dat[, ':=' (MovieID = as.numeric(MovieID), Rating = as.numeric(Rating))]
+  dat = dat[Rating > 0]
+}
+
+# read in data
+myurl = "https://liangfgithub.github.io/MovieData/"
+movies = readLines(paste0(myurl, 'movies.dat?raw=true'))
+movies = strsplit(movies, split = "::", fixed = TRUE, useBytes = TRUE)
+movies = matrix(unlist(movies), ncol = 3, byrow = TRUE)
+movies = data.frame(movies, stringsAsFactors = FALSE)
+colnames(movies) = c('MovieID', 'Title', 'Genres')
+movies$MovieID = as.integer(movies$MovieID)
+movies$Title = iconv(movies$Title, "latin1", "UTF-8")
+
+small_image_url = "https://liangfgithub.github.io/MovieImages/"
+movies$image_url = sapply(movies$MovieID, 
+                          function(x) paste0(small_image_url, x, '.jpg?raw=true'))
+
+# load Rmat and IBCF recommender
+
+load("data/Rmat.Rdata")
+
+rec = readRDS("data/ibcf_rec.rds")
+
+shinyServer(function(input, output, session) {
+  
+  # show the movies to be rated
+  output$ratings <- renderUI({
+    num_rows <- 20
+    num_movies <- 6 # movies per row
+    
+    lapply(1:num_rows, function(i) {
+      list(fluidRow(lapply(1:num_movies, function(j) {
+        list(box(width = 2,
+                 div(style = "text-align:center", img(src = movies$image_url[(i - 1) * num_movies + j], height = 150)),
+                 #div(style = "text-align:center; color: #999999; font-size: 80%", books$authors[(i - 1) * num_books + j]),
+                 div(style = "text-align:center", strong(movies$Title[(i - 1) * num_movies + j])),
+                 div(style = "text-align:center; font-size: 150%; color: #f0ad4e;", ratingInput(paste0("select_", movies$MovieID[(i - 1) * num_movies + j]), label = "", dataStop = 5)))) #00c0ef
+      })))
+    })
+  })
+  
+  # Calculate recommendations when the sbumbutton is clicked
+  df <- eventReactive(input$btn, {
+    withBusyIndicatorServer("btn", { # showing the busy indicator
+      # hide the rating container
+      useShinyjs()
+      jsCode <- "document.querySelector('[data-widget=collapse]').click();"
+      runjs(jsCode)
+      
+      # get the user's rating data
+      value_list <- reactiveValuesToList(input)
+      user_ratings <- get_user_ratings(value_list)
+      
+      # create new Rmat from user data and predict
+      n.item = ncol(Rmat)
+      new.ratings = rep(NA, n.item)
+      new.user = matrix(new.ratings, nrow=1, ncol=n.item, dimnames = list(
+                        user=paste('user'),
+                        item=colnames(Rmat)
+                        ))
+      for (i in 1:nrow(user_ratings)) {
+        movie.idx = paste0('m', user_ratings[i, 'MovieID'])
+        new.user['user', movie.idx] = as.numeric(user_ratings[i, 'Rating'])
+      }
+      new.Rmat = as(new.user, 'realRatingMatrix')
+      
+      user_results = predict(rec, new.Rmat, n=10, type='topNList')
+      user_predicted_ids = as.numeric(sub('.', '', as(user_results, "list")$user))
+      recom_results <- data.table(Rank = 1:10, 
+                                  MovieID = movies$MovieID[user_predicted_ids], 
+                                  Title = movies$Title[user_predicted_ids])
+      
+    }) # still busy
+    
+  }) # clicked on button
+  
+  
+  # display the recommendations
+  output$results <- renderUI({
+    num_rows <- 2
+    num_movies <- 5
+    recom_result <- df()
+    
+    lapply(1:num_rows, function(i) {
+      list(fluidRow(lapply(1:num_movies, function(j) {
+        box(width = 2, status = "success", solidHeader = TRUE, title = paste0("Rank ", (i - 1) * num_movies + j),
+            
+            div(style = "text-align:center", 
+                a(img(src = movies$image_url[recom_result$MovieID[(i - 1) * num_movies + j]], height = 150))
+            ),
+            div(style="text-align:center; font-size: 100%", 
+                strong(movies$Title[recom_result$MovieID[(i - 1) * num_movies + j]])
+            )
+            
+        )        
+      }))) # columns
+    }) # rows
+    
+  }) # renderUI function
+  
+}) # server function
